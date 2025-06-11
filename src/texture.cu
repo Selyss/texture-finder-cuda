7796d547afb5c9dc05884497ab42faf2f408e721
Selyss <99344963+Selyss@users.noreply.github.com>
2025-06-11 12:42:21 -0400

    add docs

--- src/texture.cu
+++ src/texture.cu
@@ -1,5 +1,12 @@
 
 // TODO: implement the other versions of the texture rotation generation
+
+/**
+ * @brief Mixes the input seed using Stafford's mix13 algorithm.
+ *
+ * @param z The input seed value.
+ * @return A pseudo-random integer derived from the input seed.
+ */
 __device__ int staffordMix13(long z)
 {
     z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9L;
@@ -7,7 +14,15 @@ __device__ int staffordMix13(long z)
     return static_cast<int>(z ^ (z >> 31));
 }
 
-// 1.13 - 1.21.1
+/**
+ * @brief Legacy texture rotation generator for versions 1.13 - 1.21.1.
+ *
+ * @param x The x-coordinate
+ * @param y The y-coordinate
+ * @param z The z-coordinate
+ * @param mod The number of rotation states
+ * @return A pseudo-random rotation state in the range [0, mod).
+ */
 __device__ int getTextureLegacy(int x, int y, int z, int mod)
 {
     constexpr int64_t multiplier = 0x5DEECE66DLL;
@@ -21,7 +36,15 @@ __device__ int getTextureLegacy(int x, int y, int z, int mod)
     return abs(rand) % mod;
 }
 
-// 1.21.2+
+/**
+ * @brief Modern texture rotation generator for versions 1.21.2 and later (at the time of writing).
+ *
+ * @param x The x-coordinate
+ * @param y The y-coordinate
+ * @param z The z-coordinate
+ * @param mod The number of rotation states
+ * @return A pseudo-random rotation state in the range [0, mod).
+ */
 __device__ int getTextureModern(int x, int y, int z, int mod)
 {
     constexpr int64_t multiplier = 0x5DEECE66DLL;
