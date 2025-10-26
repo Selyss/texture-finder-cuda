37dc6d43571628f92c309d30db53dc10c6d39f63
Selyss <99344963+Selyss@users.noreply.github.com>
2025-10-26 12:21:05 -0400

    fix sides

--- src/texture.cu
+++ src/texture.cu
@@ -7,11 +7,10 @@
  * @param z The input seed value.
  * @return A pseudo-random integer derived from the input seed.
  */
-__device__ int staffordMix13(long z)
-{
-    z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9L;
-    z = (z ^ (z >> 27)) * 0x94D049BB133111EBL;
-    return static_cast<int>(z ^ (z >> 31));
+__device__ int staffordMix13(long z) {
+  z = (z ^ (z >> 30)) * 0xBF58476D1CE4E5B9L;
+  z = (z ^ (z >> 27)) * 0x94D049BB133111EBL;
+  return static_cast<int>(z ^ (z >> 31));
 }
 
 /**
@@ -23,21 +22,21 @@ __device__ int staffordMix13(long z)
  * @param mod The number of rotation states
  * @return A pseudo-random rotation state in the range [0, mod).
  */
-__device__ int getTextureLegacy(int x, int y, int z, int mod)
-{
-    constexpr int64_t multiplier = 0x5DEECE66DLL;
-    constexpr int64_t mask = (1LL << 48) - 1;
-    int64_t l = (static_cast<int64_t>(x * 3129871)) ^
-                (static_cast<int64_t>(z) * 116129781L) ^ static_cast<int64_t>(y);
-    l = l * l * 42317861L + l * 11L;
-    int64_t seed = l >> 16;
-    seed = (seed ^ multiplier) & mask;
-    int rand = static_cast<int>((seed * 0xBB20B4600A69L + 0x40942DE6BAL) >> 16);
-    return abs(rand) % mod;
+__device__ int getTextureLegacy(int x, int y, int z, int mod) {
+  constexpr int64_t multiplier = 0x5DEECE66DLL;
+  constexpr int64_t mask = (1LL << 48) - 1;
+  int64_t l = (static_cast<int64_t>(x * 3129871)) ^
+              (static_cast<int64_t>(z) * 116129781L) ^ static_cast<int64_t>(y);
+  l = l * l * 42317861L + l * 11L;
+  int64_t seed = l >> 16;
+  seed = (seed ^ multiplier) & mask;
+  int rand = static_cast<int>((seed * 0xBB20B4600A69L + 0x40942DE6BAL) >> 16);
+  return abs(rand) % mod;
 }
 
 /**
- * @brief Modern texture rotation generator for versions 1.21.2 and later (at the time of writing).
+ * @brief Modern texture rotation generator for versions 1.21.2 and later (at
+ * the time of writing).
  *
  * @param x The x-coordinate
  * @param y The y-coordinate
@@ -45,16 +44,16 @@ __device__ int getTextureLegacy(int x, int y, int z, int mod)
  * @param mod The number of rotation states
  * @return A pseudo-random rotation state in the range [0, mod).
  */
-__device__ int getTextureModern(int x, int y, int z, int mod)
-{
-    constexpr int64_t multiplier = 0x5DEECE66DLL;
-    constexpr int64_t mask = (1LL << 48) - 1;
-    int64_t l = (static_cast<int64_t>(x * 3129871)) ^
-                (static_cast<int64_t>(z) * 116129781L) ^ static_cast<int64_t>(y);
-    l = l * l * 42317861L + l * 11L;
-    int64_t seed = l >> 16;
-    seed = (seed ^ multiplier);
-    seed = seed * multiplier + 11LL & mask;
-    int next = static_cast<int>(seed >> (48 - 31));
-    return static_cast<int>((mod * static_cast<int64_t>(next)) >> 31);
+__device__ int getTextureModern(int x, int y, int z, int mod) {
+  constexpr int64_t multiplier = 0x5DEECE66DLL;
+  constexpr int64_t mask = (1LL << 48) - 1;
+  int64_t l = (static_cast<int64_t>(x * 3129871)) ^
+              (static_cast<int64_t>(z) * 116129781L) ^ static_cast<int64_t>(y);
+  l = l * l * 42317861L + l * 11L;
+  int64_t seed = l >> 16;
+  seed = (seed ^ multiplier);
+  seed = seed * multiplier + 11LL & mask;
+  int next = static_cast<int>(seed >> (48 - 31));
+  // i hope this does not explode
+  return static_cast<int>(((4LL * next) >> 31) % mod);
 }
